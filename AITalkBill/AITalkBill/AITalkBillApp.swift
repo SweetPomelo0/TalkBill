1b1c1fa9ebce1c7844541713c0a6e1f64b951b52
SweetPomelo0 <shenghuicc@qq.com>
2025-06-27 00:01:47 +0800

    Initial commit from local Xcode project

---
+++ AITalkBill/AITalkBill/AITalkBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  AITalkBillApp.swift
+//  AITalkBill
+//
+//  Created by cuteerha on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct AITalkBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AITalkBill/AITalkBill/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AITalkBill
+//
+//  Created by cuteerha on 6/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("欢迎来到 AI 记账 App 🧾")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
